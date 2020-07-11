78a90a587621216a0e475c8d4617e37f37ee6b2b
Yvette <nchombuayvta@gmil.com>
2020-07-11 09:19:23 +0200

    new item created

--- models/routes/api/items.js
+++ models/routes/api/items.js
@@ -3,23 +3,39 @@ const router = express.Router();
 
 
 //item model
-const item = require('../../schema');
+const Item = require('../../schema');
 
 // @ route get request api/items
 //sol1
 router.get('/',  async(req,res)=>{
-    const items = await item.find(req.query)
+    const items = await Item.find(req.query)
+    res.status(200).send(items)
 })
 
 // sol2
 router.get('/', (req,res) =>{
-    const items = item.find()
+    const items = Item.find()
     .sort({date:-1})
     // the database we return us the items
     .then(items => res.json(items))
 })
 
+// create a post
+//sol 1
+router.post('/', (req,res)=>{
+    const newItem = new Item({
+        name: req.body.name
+    })
+    newItem.save()
+    .then(item => res.json(item))
+})
+// sol2
+router.post('/', async(req,res)=>{
+    const newItem = await new Item(req.body.name)
+    const response = await newItem.save()
+    res.status(201).send(response)
 
+})
 
 
 
--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const express = require ("express")
 const mongoose = require("mongoose")
 const bodyParser = require ('body-parser')
 
+const items = require('./models/routes/api/items')
+
+
 
 const server = express()
 
 server.use (bodyParser.json())
+
+// connect to database
 mongoose.connect("mongodb://localhost:27017/mern_shopping",{
     useNewUrlParser:true,
     useUnifiedTopology:true
+})
+
+// use routes
+server.use ('/api/items', items)
+const port = process.env.PORT || 5000
+server.listen(port, () =>{
+    console.log(`server started on port ${port}`)
 })
